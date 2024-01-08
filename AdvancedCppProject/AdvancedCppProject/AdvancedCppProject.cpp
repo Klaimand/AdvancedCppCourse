6d01e908f3ff0e4b1bade00d94f09f752f47ffe7
Clément Pagnier <clempagnier@yahoo.fr>
2024-01-08 09:06:54 +0100

    typeid

--- AdvancedCppProject/AdvancedCppProject/AdvancedCppProject.cpp
+++ AdvancedCppProject/AdvancedCppProject/AdvancedCppProject.cpp
@@ -1,43 +1,15 @@
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <typeinfo>
 using namespace std;
-
-bool match(string test)
-{
-    return test.size() == 3;
-}
-
-template<typename T, typename K>
-int countIf(T begin, T end, bool (*fn)(K))
-{
-    int i = 0;
-    for (T it = begin; it != end; it++)
-    {
-        if (fn(*it))
-            i++;
-    }
-
-    return i;
-}
 
 int main()
 {
 
-    vector<string> texts;
-    texts.push_back("one");
-    texts.push_back("two");
-    texts.push_back("three");
-    texts.push_back("two");
-    texts.push_back("four");
-    //texts.push_back("two");
-    texts.push_back("three");
+    string value;
 
-    //cout << match("oned") << endl;
+    cout << typeid(value).name() << endl;
 
-    //cout << count_if(texts.begin(), texts.end(), match) << endl;
-    cout << countIf(texts.begin(), texts.end(), match) << endl;
 
     return 0;
 }
